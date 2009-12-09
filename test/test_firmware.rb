f0a2f251770e45669bbf64582a6dbad5071b0b63
Evan Farrar <evanfarrar@gmail.com>
2009-12-09 13:54:08 -0600

    Tests are passing with latest firmware. Also now you can specify the port you'd like to use.

--- test/test_firmware.rb
+++ test/test_firmware.rb
@@ -4,21 +4,21 @@ require 'timeout'
 
 describe 'Basic Message Firmware' do
   before do
-    filename = "/dev/ttyUSB0"
+    filename = ENV['OPENSPRINTS_PORT']||"/dev/ttyUSB0"
     raise "Can't find the arduino" unless File.writable?(filename)
     `stty -F #{filename} cs8 115200 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts`
     @serialport = File.open(filename, "w+")
   end
 
   after do
-    @serial.close
+    @serialport.close
   end
 
   it "should respond to 'v' with its version" do
     @serialport.putc ?v
     @serialport.putc ?\n
     Timeout.timeout(2) {
-      @serialport.readline.should==("basic-1")
+      @serialport.readline.should==("basic-1\r\n")
     }
   end
 end
