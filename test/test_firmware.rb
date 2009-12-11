44ded1a5b88f84a77e4ccf3f2a03e644001ebbe8
LJO <orluke@gmail.com>
2009-12-10 18:57:28 -0600

    now flags variable

--- test/test_firmware.rb
+++ test/test_firmware.rb
@@ -5,12 +5,9 @@ require 'timeout'
 describe 'Basic Message Firmware' do
   before do
     filename = ENV['OPENSPRINTS_PORT']||"/dev/ttyUSB0"
+	flags = "406:0:18b2:8a30:3:1c:7f:8:4:2:64:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0"
     raise "Can't find the arduino" unless File.writable?(filename)
-
-
-
-
-    `stty -F #{filename}"406:0:18b2:8a30:3:1c:7f:8:4:2:64:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0"`
+    `stty -F #{filename} #{flags}`
     @serialport = File.open(filename, "w+")
   end
 
