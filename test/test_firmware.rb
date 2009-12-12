89b344b336142fff33b8a3cde6235a83ba5ec892
Evan Farrar <evanfarrar@gmail.com>
2009-12-11 18:43:30 -0600

    Some black magic.

--- test/test_firmware.rb
+++ test/test_firmware.rb
@@ -1,14 +1,21 @@
 require 'rubygems'
 require 'bacon'
 require 'timeout'
+include Timeout
+#prime the serial port
+filename = ENV['OPENSPRINTS_PORT']||"/dev/ttyUSB0"
+serialport = File.open(filename, "w+")
+serialport.close
+sleep(2)
 
 describe 'Basic Message Firmware' do
   before do
     filename = ENV['OPENSPRINTS_PORT']||"/dev/ttyUSB0"
-	flags = "406:0:18b2:8a30:3:1c:7f:8:4:2:64:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0"
+    flags = "406:0:18b2:8a30:3:1c:7f:8:4:2:64:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0"
     raise "Can't find the arduino" unless File.writable?(filename)
     `stty -F #{filename} #{flags}`
     @serialport = File.open(filename, "w+")
+    @serialport.flush
   end
 
   after do
@@ -18,7 +25,7 @@ describe 'Basic Message Firmware' do
   it "should respond to 'v' with its version" do
     @serialport.putc ?v
     @serialport.putc ?\n
-    Timeout.timeout(2) {
+    timeout(0.1) {
       @serialport.readline.should==("basic-1\r\n")
     }
   end
