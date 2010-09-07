16cad6273d82bb47c07659ef2355a71457ec2792
LJO <orluke@gmail.com>
2010-09-07 05:37:22 -0400

    testing countdown - there is a NACK being caused.

--- test/test_firmware.rb
+++ test/test_firmware.rb
@@ -17,14 +17,28 @@ raise "Can't find the arduino" unless File.writable?(filename)
 `stty -F #{filename} #{flags}`
 serialport = File.open(filename, "w+")
 serialport.flush
+serialport.close
 sleep(1.5)
 
 # Get Racemonitor back to Idle State from any other state
 # and reset all API-writeable values to their system defaults.
-serialport.write "!defaults\r\n"
-timeout(0.1) {
+serialport = File.open(filename, "w+")
+serialport.write "!s\r\n"
+serialport.flush
+timeout(0.2) {
 	puts serialport.readline
 }
+
+serialport.close
+
+serialport = File.open(filename, "w+")
+
+serialport.write "!defaults\r\n"
+serialport.flush
+timeout(0.2) {
+	puts serialport.readline
+}
+
 
 idle_stimulus_and_response = [
   ["Test a handshake boundary case\n",  "!a:65536\r\n", "A:NACK\r\n"], 
@@ -41,10 +55,13 @@ idle_stimulus_and_response = [
   ["Test a countdown boundary case:\n", "!c:256\r\n",   "C:NACK\r\n"], 
   ["Test the hw command with a valid message:\n",   "!hw\r\n","HW:3\r\n"],
   ["Test the racer enable command with a malformed message:\n",   "!i\r\n","NACK\r\n"],
-  ["Test the racer enable command with a boundary case:\n",               "!i:0\r\n","I:NACK\r\n"],
-  ["Test the racer enable command with a valid message:\n",  "!i:6\r\n","I:6\r\n"],
-  ["Test the racer enable command with a valid message:\n",  "!i:4294967295\r\n","I:4294967295\r\n"],
+  ["Test the racer enable command with a boundary case:\n",  "!i:0\r\n","I:NACK\r\n"],
   ["Test the racer enable command with a boundary case:\n",  "!i:4294967296\r\n","I:NACK\r\n"],
+  ["Test the racer enable command with a valid message:\n",  "!i:6\r\n","I:ERROR\r\n"],
+  ["Test the racer enable command with a valid message:\n",  "!i:4294967295\r\n","I:ERROR\r\n"],
+# Replace the above 2 tests with the following 2 sets when the i: command gets implemented.
+#  ["Test the racer enable command with a valid message:\n",  "!i:6\r\n","I:6\r\n"],
+#  ["Test the racer enable command with a valid message:\n",  "!i:4294967295\r\n","I:4294967295\r\n"],
   ["Test the race length command with a malformed message:\n",   "!l\r\n","NACK\r\n"],
   ["Test the race length command with a valid message:\n",   "!l:0\r\n","L:0\r\n"],
   ["Test the race length command with a valid message:\n",   "!l:65535\r\n","L:65535\r\n"],
@@ -60,7 +77,10 @@ idle_stimulus_and_response = [
   ["Test the protocol request command with a valid message:\n",   "!p\r\n","P:1.02\r\n"],
   ["Test the protocol request command with a malformed message:\n",   "!p:023\r\n","NACK\r\n"],
   ["Test the fw version request command with a valid message:\n",   "!v\r\n","V:1.02\r\n"],
-  ["Test the fw version request command with a malformed message:\n",   "!v:555\r\n","NACK\r\n"]
+  ["Test the fw version request command with a malformed message:\n",   "!v:555\r\n","NACK\r\n"],
+  ["Test the Go command with a malformed message:\n",   "!g:\r\n",  "NACK\r\n"],
+  ["Test the Go command with a boundary case:\n",       "!g:1098\r\n","NACK\r\n"],
+  ["Test the Go command with a malformed message:\n",   "!g:$!&#\r\n","NACK\r\n"],
 ]
 
 
@@ -91,41 +111,41 @@ idle_stimulus_and_response.each do |descr,stimulus,expected_response|
 
 end
 
-countdown_stimulus_and_response = [
-  ["Test a countdown valid case:\n",    "!c:4\r\n",     "C:4\r\n"],
-  ["Test the Go command with a malformed message:\n",   "!g:\r\n",  "NACK\r\n"],
-  ["Test the Go command with a boundary case:\n",       "!g:1098\r\n","NACK\r\n"],
-  ["Test the Go command with a malformed message:\n",   "!g:$!&#\r\n","NACK\r\n"],
-#  ["Test the Go command while in idle state:\n",    "!g\r\n",   "G\r\n"], 
-#  ["Test the Stop command while in countdown state:\n",           "!s\r\n","S\r\n"],
-#  ["Test the Stop command with a malformed message:\n",      "!s:0\r\n","NACK\r\n"],
-]
+def write_command(command)
+  filename = ENV['OPENSPRINTS_PORT']||"/dev/ttyUSB0"
+  flags = "406:0:18b2:8a30:3:1c:7f:8:4:2:64:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0"
+  raise "Can't find the arduino" unless File.writable?(filename)
+  `stty -F #{filename} #{flags}`
+  @serialport = File.open(filename, "w+")
+  @serialport.write command
+  @serialport.close
+  @serialport = File.open(filename, "w+")
+end
 
+describe "countdown" do
+  before do
+    write_command("!defaults\r\n")
 
-countdown_stimulus_and_response.each do |descr,stimulus,expected_response|
+    timeout(1.1) {
+      puts @serialport.readline
+    }
+  end
+  after do
+  end
 
-	describe descr do
-		before do
-			filename = ENV['OPENSPRINTS_PORT']||"/dev/ttyUSB0"
-			flags = "406:0:18b2:8a30:3:1c:7f:8:4:2:64:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0"
-			raise "Can't find the arduino" unless File.writable?(filename)
-			`stty -F #{filename} #{flags}`
-			@serialport = File.open(filename, "w+")
-			@serialport.flush
-		end
-
-		after do
-			@serialport.close
-		end
-
-		should "respond with '" + expected_response.strip + "\\r\\n" + "' to '" + stimulus.strip + "\\r\\n" + "'" do
-			@serialport.write stimulus
-			@serialport.flush
-			timeout(0.1) {
-				@serialport.readline.should==(expected_response)
-			}
-		end
-	end
-
+  it "should count down to zero, defaulting to 4" do
+    @serialport.flush
+    write_command("!g\r\n") 
+    timeout(0.1) {
+      @serialport.readline.should == "G\r\n"
+    }
+    result = ''
+    timeout(6) do
+      6.times do
+        result += @serialport.readline
+      end
+      result.should.include("CD:4\r\nCD:3\r\nCD:2\r\nCD:1\r\nCD:0\r\n")
+    end
+  end
 end
 
