759c7c3e999efbb75fc423425ab816e740cd3d41
Luke Orland <orluke@gmail.com>
2011-01-14 01:54:23 -0500

    in POSIX, this test script can automatically detect the port that the arduino is mounted to and query the fw version.

--- get_fw_version.rb
+++ get_fw_version.rb
@@ -1,38 +1,39 @@
 require 'rubygems'
 require 'timeout'
 include Timeout
+require 'serialport'
+
+def getVersion(devLoc)
+  #params for serial port
+  port_str = devLoc  #may be different for you
+  baud_rate = 115200
+  data_bits = 8
+  stop_bits = 1
+  parity = SerialPort::NONE
+
+  sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+
+  # The ATmega reboots once connection is made to the FTDI part.
+  # So wait for it to come back up before sending anything.
+  sleep(2)
+  timeout(4) {
+    sp.write "!v\r\n"
+    puts sp.readline
+  }
+
+  sp.close                       #see note 1
+  
+  #raise "Can't find the arduino" unless File.writable?(devLoc)
+end
 
 # Prime the serial port
 # First time opening the device (port / file / tty) after plugging in the 
 # Arduino causes it to reboot.
-filename = Dir.glob("/dev/tty{.usb,USB}*")
-puts filename
-=begin
-flags = "406:0:18b2:8a30:3:1c:7f:8:4:2:64:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0"
-raise "Can't find the arduino" unless File.writable?(filename)
-`stty -F #{filename} #{flags}`
-serialport = File.open(filename, "w+")
-serialport.flush
-serialport.close
-sleep(1.5)
+mntLoc = Dir.glob("/dev/tty{.usb,USB}*")[0]
+if mntLoc == nil
+  puts "nothing attached to an expected mount point."
+else
+  puts mntLoc
+  getVersion(mntLoc)
+end
 
-# Get Racemonitor back to Idle State from any other state
-# and reset all API-writeable values to their system defaults.
-serialport = File.open(filename, "w+")
-serialport.write "!s\r\n"
-serialport.flush
-timeout(0.2) {
-	puts serialport.readline
-}
-
-serialport.close
-
-serialport = File.open(filename, "w+")
-
-serialport.write "!defaults\r\n"
-serialport.flush
-timeout(0.2) {
-	puts serialport.readline
-}
-
-=end
