a713900f858524a4ec51120a986f12ad96bdde54
Luke Orland <orluke@gmail.com>
2013-05-09 00:39:11 -0400

    playing with serproxy from ruby script

---
+++ derproxy.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+
+serproxy_dir = "/Users/orluke/Downloads/tinkerproxy-2_0"
+# TODO: determine the comm port 
+pid = spawn("#{serproxy_dir}/serproxy.osx #{serproxy_dir}/serproxy.osx.cfg")
+
+sleep(1)
+
+sock = TCPSocket.new("localhost", 5331)
+sleep(2)
+sock.write "!v\n"
+#puts sock.gets("\r\n")
+#puts sock.readline("\r\n")
+puts sock.recv(9)
+sock.close
+
+sleep(1)
+Process.kill(9, pid)
+
